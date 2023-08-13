bda6ba7186c82469fe19417c2e2e5a3a07aaacb0
pnorbi01 <pnorbyy01@gmail.com>
2023-08-13 11:19:20 +0200

    Added profile check

--- Notifications.js
+++ Notifications.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, Image, SafeAreaView, TouchableOpacity, FlatList } from 'react-native';
+import { StyleSheet, View, Text, Image, SafeAreaView, TouchableOpacity, FlatList, StatusBar } from 'react-native';
 import { formatDistanceToNow } from 'date-fns';
 import { enGB } from 'date-fns/locale';
 import { useFocusEffect } from '@react-navigation/native';
+import Modal from 'react-native-modal';
 
 
 const seperator = () => {
@@ -17,6 +18,11 @@ const Notifications = ({route, navigation}) => {
     const [data, setData] = useState([]);
     const [isCircleVisible, setIsCircleVisible] = useState({});
     const [message, setMessage] = useState('');
+    const [isModalVisible, setModalVisible] = useState(false);
+    const [modalImage, setModalImage] = useState('');
+    const [modalUsername, setModalUsername] = useState('');
+    const [modalLevel, setModalLevel] = useState('');
+
 
     useFocusEffect(
         React.useCallback(() => {
@@ -24,6 +30,20 @@ const Notifications = ({route, navigation}) => {
           console.log("Notifik");
         }, [])
     );
+
+    const toggleModal = (modalImage, modalUsername, modalLevel) => {
+        setModalImage(modalImage);
+        setModalUsername(modalUsername);
+        setModalLevel(modalLevel);
+        setModalVisible(!isModalVisible);
+    };
+
+    const closeModal = () => {
+        setModalImage('');
+        setModalUsername('');
+        setModalLevel('');
+        setModalVisible(false);
+    };
 
     const listNotifications = async () => {
         const response = await fetch('http://192.168.0.17/EventRain/api/events/read-notifications.php', {
@@ -88,8 +108,10 @@ const Notifications = ({route, navigation}) => {
                                             Event Invite
                                         </Text>
                                         <View style={styles.locationFlat}>
-                                            <Image source={{ uri: 'https://printf.stud.vts.su.ac.rs/EventRain/assets/images/profile-pictures/'+ item.image }} style={{ width: 20, height: 20, borderRadius: 50 }} /> 
-                                            <Text style={{color: '#A9A9A9', marginLeft: 5}}>{item.username}</Text>
+                                            <TouchableOpacity style={styles.locationFlat} onPress={() => toggleModal(item.image, item.username, item.level)}>
+                                                <Image source={{ uri: 'https://printf.stud.vts.su.ac.rs/EventRain/assets/images/profile-pictures/'+ item.image }} style={{ width: 20, height: 20, borderRadius: 50 }} /> 
+                                                <Text style={{color: '#A9A9A9', marginLeft: 5}}>{item.username}</Text>
+                                            </TouchableOpacity>
                                         </View>
                                     </View>
                                     {data.length > 0 && <Text style={{color: '#a9a9a9'}}>{formattedDate}</Text>}
@@ -103,6 +125,38 @@ const Notifications = ({route, navigation}) => {
                 ItemSeparatorComponent={seperator}
             />
             )}
+            <Modal
+                onBackdropPress={() => setModalVisible(false)}
+                onBackButtonPress={() => setModalVisible(false)}
+                isVisible={isModalVisible}
+                swipeDirection="down"
+                onSwipeComplete={closeModal}
+                animationIn="slideInUp"
+                animationOut="slideOutDown"
+                animationInTiming={900}
+                animationOutTiming={500}
+                backdropTransitionInTiming={1000}
+                backdropTransitionOutTiming={500}
+                style={styles.modal}
+            >
+                <View style={styles.modalContent}>
+                    <View style={styles.barIcon} />
+                    <View style={styles.accountInformation}>
+                        <Image source={{ uri: 'https://printf.stud.vts.su.ac.rs/EventRain/assets/images/profile-pictures/'+ modalImage }} style={{ width: 100, height: 100, borderRadius: 50 }} />
+                        <Text style={styles.text}>{modalUsername}</Text>
+                    </View>
+                    {modalLevel === 'admin' ? (
+                        <View style={styles.accountDescriptionVerified}>
+                            <Image source={require('./assets/verified.png')} style={{width: 20, height: 20}}/>
+                            <Text style={{color: '#FFF', marginLeft: 5}}>The following account is verified, bacause is a developer at EventRain.</Text>
+                        </View>
+                    ) : (
+                        <View style={styles.accountDescriptionNotVerified}>
+                            <Text style={{color: '#FFF'}}>The following account is not verified.</Text>
+                        </View>
+                    )}
+                </View>
+            </Modal>
         </SafeAreaView>
   );
 };
@@ -174,6 +228,71 @@ const styles = StyleSheet.create({
         fontWeight: '200',
         fontSize: 15,
         marginTop: 10
+    },
+
+    modal: {
+        justifyContent: "flex-end",
+        margin: 0,
+    },
+    
+    modalContent: {
+        backgroundColor: "#161616",
+        paddingTop: 12,
+        paddingHorizontal: 12,
+        borderTopRightRadius: 20,
+        borderTopLeftRadius: 20,
+        minHeight: 300,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'center'
+    },
+    
+    center: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+      
+    barIcon: {
+        width: 60,
+        height: 5,
+        backgroundColor: "#bbb",
+        borderRadius: 3,
+        position: 'absolute',
+        top: 10
+    },
+      
+    text: {
+        color: "#bbb",
+        fontSize: 24,
+        marginTop: 10
+    },
+
+    accountInformation: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 25
+    },
+
+    accountDescriptionVerified: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        width: '100%',
+        padding: 25
+    },
+
+    accountDescriptionNotVerified: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        width: '100%',
+        padding: 25
     }
   
 });
